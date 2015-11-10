52002e7e4b742a333da70d2222b60de0b5f68a82
Nathan Vahrenberg <nvahren@gregvahergsmbp4.dhcp.nd.edu>
2015-11-10 09:58:09 -0500

    validation added

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,6 @@
 class List < ActiveRecord::Base
 	has_many :tasks
+	validates_associated :tasks
+	validates :title, presence: true, uniqueness: true
+	validates :description, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
 	belongs_to :list
+	validates :title, presence: true
+	validates :priority, presence: true, :inclusion => { :in => 1..10 }, numericality: { only_integer: true }
+	validates :due_date, presence: true
+	validates :list_name, presence: true
 end
