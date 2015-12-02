fc946294ade9371e735e4a8c575c9f4e34427e3a
Nathan Vahrenberg <nvahren@Greg-Vahrenbergs-MacBook-Pro-4.local>
2015-12-02 16:37:56 -0500

    all but css done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+	before_action :get_all_task_lists
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def get_all_task_lists
+  	@all_task_lists = Array.new
+  	TaskList.all.each do |item|
+  		@all_task_lists.push(item)
+  	end
+  end
+
 end
