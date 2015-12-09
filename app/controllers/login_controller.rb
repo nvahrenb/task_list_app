eecf686bc82b3cefaa705019dcda8a29a2419ea3
Nathan Vahrenberg <nvahren@gregvahergsmbp4.dhcp.nd.edu>
2015-12-09 11:17:01 -0500

    login/logout working

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,8 +3,20 @@ class LoginController < ApplicationController
   end
 
   def authenticate
+  	user = User.find_by_username(params[:username])
+  	user2 = User.find_by_password(params[:password])
+  	if user == user2 && !user.nil?
+  		session[:user_id] = user.id
+  		flash[:message] = "Logged in"
+  		redirect_to root_url
+  	else
+  		flash[:message] = "Username/password not found"
+  		redirect_to :back
+  	end
   end
 
   def logout
+  	session[:user_id] = nil
+  	redirect_to login_path
   end
 end
--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -2,8 +2,9 @@ class ProtectedController < ApplicationController
 	before_action :require_login
 
 	def require_login
-		if !session[:user_id]
-			redirect_to login_path, :flash => { :login => "User not logged in" }
+		if session[:user_id].nil?
+			flash[:message] = "User not logged in"
+			redirect_to login_path
 		end
 	end
 end
